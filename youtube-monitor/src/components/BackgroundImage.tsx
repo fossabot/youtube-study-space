f10a3fdb876161a53eb48bd2484c8f56e0bb4341
sorarideblog <sorarideblog@gmail.com>
2022-07-21 12:38:44 +0900

    Update BackgroundImage.tsx

--- youtube-monitor/src/components/BackgroundImage.tsx
+++ youtube-monitor/src/components/BackgroundImage.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { getCurrentSection } from '../lib/time_table'
 import * as styles from '../styles/BackgroundImage.styles'
 
-class BackgroundImage extends React.Component<Record<string, unknown>, any> {
-    private intervalId: NodeJS.Timeout | undefined
-    private base_url = 'https://source.unsplash.com/featured/1920x1080'
-    private args: string =
+const BackgroundImage: FC = () => {
+    const base_url = 'https://source.unsplash.com/featured/1920x1080'
+    const args: string =
         '/?' +
         'work,cafe,study,nature,chill,coffee,tea,sea,lake,outdoor,land,spring,summer,fall,winter,hotel' +
         ',green,purple,pink,blue,dark,azure,yellow,orange,gray,brown,red,black,pastel' +
@@ -13,49 +12,46 @@ class BackgroundImage extends React.Component<Record<string, unknown>, any> {
         ',pencil,pen,eraser,stationary,classic,jazz,lo-fi,fruit,vegetable,sky,instrument,cup' +
         ',star,moon,night,cloud,rain,mountain,river,calm,sun,sunny,water,building,drink,keyboard' +
         ',morning,evening'
-    private unsplash_url = this.base_url + this.args
+    const unsplash_url = base_url + args
 
-    updateState() {
+    const intervalRef = useRef<NodeJS.Timeout>()
+    const [srcUrl, setSrcUrl] = useState<string>('')
+    const [lastPartName, setLastPartName] = useState<string>()
+
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            updateState()
+        }, 1000)
+
+        setSrcUrl(unsplash_url)
+        setLastPartName('')
+    }, []) // マウント時のみ
+
+    useEffect(() => {
+        // do something
+        console.log()
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current)
+            }
+        }
+    }, [])
+
+    const updateState = () => {
         const now = new Date()
         const currentSection = getCurrentSection()
 
-        if (currentSection?.partType !== this.state.lastPartName) {
-            this.setState({
-                srcUrl: `${this.unsplash_url},${now.getTime()}`,
-                lastFetchedDate: now.getDate(),
-                lastPartName: currentSection?.partType,
-            })
+        if (currentSection?.partType !== lastPartName) {
+            setSrcUrl(`${unsplash_url},${now.getTime()}`)
+            setLastPartName(currentSection?.partType)
         }
     }
 
-    constructor(props: Record<string, unknown>) {
-        super(props)
-        this.state = {
-            srcUrl: this.unsplash_url,
-            lastFetchedDate: new Date().getDate(),
-            lastPartName: '',
-        }
-    }
-
-    componentDidMount() {
-        this.intervalId = setInterval(() => {
-            this.updateState()
-        }, 1000)
-    }
-
-    componentWillUnmount() {
-        if (this.intervalId) {
-            clearInterval(this.intervalId)
-        }
-    }
-
-    render() {
-        return (
-            <div css={styles.backgroundImage}>
-                <img src={this.state.srcUrl} alt='背景画像' />
-            </div>
-        )
-    }
+    return (
+        <div css={styles.backgroundImage}>
+            <img src={srcUrl} alt='背景画像' />
+        </div>
+    )
 }
 
 export default BackgroundImage
